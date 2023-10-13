0c723c6edfa7da2df235fffa3146c0b2cb09b913
parth thakor <parththakor2610@gmail.com>
2023-10-13 13:12:28 +0530

    delete button added

--- 01create_react_app/src/components/Expenses/ExpenseItem.js
+++ 01create_react_app/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,24 @@
 import  './ExpenseItem.css'
+// import {useState} from 'react'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Cart from '../UI/Card';
-const ExpenseItem =  ({title , date , amount ,LocationOfExpenditure}) =>{
- 
+const ExpenseItem =  ({index,title , date , amount ,LocationOfExpenditure , onDeleteExpense}) =>{
+
+  // let newtitle = title
+    const clickHandler = ()=>{
+  //  newtitle = "updated title"
+  // setTtile('updatedtitle1')
+  //  console.log(newtitle)
+  onDeleteExpense(index);
+
+  }
   return(
    <Cart className="expense-item">
     <ExpenseDate date ={date}/>
-    <ExpenseDetails amount={amount} title={title} LocationOfExpenditure={LocationOfExpenditure}/>    
+    <ExpenseDetails amount={amount} title={title} LocationOfExpenditure={LocationOfExpenditure}/>
+    <button onClick={clickHandler}>click me</button>    
+    {/* <button onClick={()=>{console.log("click")}}>click me</button>     */}
    </Cart>
   );
 
--- 01create_react_app/src/components/Expenses/Expenses.js
+++ 01create_react_app/src/components/Expenses/Expenses.js
@@ -1,15 +1,34 @@
 import Cart from '../UI/Card'
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
+import { useState } from 'react'
 
 const Expenses =({expenses})=>{
+  const [expenseList , setExpenses] = useState(expenses);
+  
+  const deleteExpenseHandler = (id)=>{
+    console.log(id);
+ const updatedExpenses = expenseList.filter((expense , i)=> i !== id)
+ console.log(updatedExpenses)
+ console.log(expenses)
+ setExpenses(updatedExpenses);
+  }
   return (
    <Cart className='expenses'>
-    <ExpenseItem  title ={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} LocationOfExpenditure ={expenses[0].LocationOfExpenditure}/>
-      <ExpenseItem  title ={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} LocationOfExpenditure ={expenses[1].LocationOfExpenditure}/>
-      <ExpenseItem  title ={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} LocationOfExpenditure ={expenses[2].LocationOfExpenditure}/>
-      <ExpenseItem  title ={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} LocationOfExpenditure ={expenses[3].LocationOfExpenditure}/>
-   </Cart>
+  {
+    expenses.map((expense , index)=>(
+      <ExpenseItem 
+       key={index}
+       index = {index}
+       title ={expense.title} 
+       amount={expense.amount} 
+       date={expense.date} 
+       LocationOfExpenditure ={expense.LocationOfExpenditure}
+       onDeleteExpense = {deleteExpenseHandler}
+       />
+    ))
+  } 
+  </Cart>
   )
 }
 export default Expenses;
