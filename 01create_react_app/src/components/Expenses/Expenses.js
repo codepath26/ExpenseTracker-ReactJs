02a24bedaf8a8258e049ae21ef8c8bf17c485015
parth thakor <parththakor2610@gmail.com>
2023-10-14 23:37:13 +0530

    learning about the conditional rendering tips and tricks

--- 01create_react_app/src/components/Expenses/Expenses.js
+++ 01create_react_app/src/components/Expenses/Expenses.js
@@ -1,44 +1,51 @@
-import Cart from '../UI/Card'
-import './Expenses.css'
-import ExpenseItem from './ExpenseItem'
-import { useState } from 'react'
-import ExpenseFilter from './ExpenseFilter'
+import Cart from "../UI/Card";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
 
-const Expenses =({expenses})=>{
-  const [expenseList , setExpenses] = useState(expenses);
-  const [filteredYear , setFilteredYear] = useState('2020');
+const Expenses = ({ expenses }) => {
+  const [expenseList, setExpenses] = useState(expenses);
+  const [filteredYear, setFilteredYear] = useState("2020");
   
-  const filterChangeHandler = (selectedYear)=>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+  };
+  
+   
+  const deleteExpenseHandler = (id) => {
+    console.log(id);
+    const updatedExpenses = expenseList.filter((expense, i) => i !== id);
+    console.log(updatedExpenses);
+    console.log(expenses);
+    setExpenses(updatedExpenses);
+  };
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  let expensesContent = <p>Exepenses not Found</p>
+  if(filteredExpenses.length > 0){
+     expensesContent = filteredExpenses.map((expense, index) => (
+      <ExpenseItem
+        key={expense.id}
+        index={index}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        //  LocationOfExpenditure ={expense.LocationOfExpenditure}
+        onDeleteExpense={deleteExpenseHandler}
+      />
+    ));    
   }
 
-  const deleteExpenseHandler = (id)=>{
-    console.log(id);
- const updatedExpenses = expenseList.filter((expense , i)=> i !== id)
- console.log(updatedExpenses)
- console.log(expenses)
- setExpenses(updatedExpenses);
-  }
-  const filteredExpenses = expenses.filter(expense =>{
-    return expense.date.getFullYear().toString() === filteredYear;
-  })
   return (
-   <Cart className='expenses'>
-    <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
-  {
-    filteredExpenses.map((expense , index)=>(
-      <ExpenseItem 
-       key={expense.id}
-       index = {index}
-       title ={expense.title} 
-       amount={expense.amount} 
-       date={expense.date} 
-      //  LocationOfExpenditure ={expense.LocationOfExpenditure}  
-       onDeleteExpense = {deleteExpenseHandler}
-       />
-    ))
-  } 
-  </Cart>
-  )
-}
+    <Cart className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent};
+    </Cart>
+  );
+};
 export default Expenses;
